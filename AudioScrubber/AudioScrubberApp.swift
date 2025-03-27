6b7257783c1020992cb02e9c2fa4620e5f15dfad
parisollie <pjff18@gmail.com>
2025-03-27 17:57:35 -0600

    Initial Commit

---
+++ AudioScrubber/AudioScrubberApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioScrubberApp.swift
+//  AudioScrubber
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioScrubberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
